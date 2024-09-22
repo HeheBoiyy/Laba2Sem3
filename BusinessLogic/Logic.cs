660b6f7321e8f50f194d1a1051dbf8bcacd5936a
HeheBoiyy <school152qq@mail.ru>
2024-09-22 16:21:03 +0700

    Добавьте файлы проекта.

---
+++ BusinessLogic/Logic.cs
@@ -0,0 +1,141 @@
+﻿using Model;
+using DataAccessLayer;
+namespace BusinessLogic
+{
+    public class Logic
+    {
+        IRepository<Student> repository = new DapperRepository<Student>();
+        /// <summary>
+        /// Метод получения студента по ID
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>Студент</returns>
+        public List<string> GetStudent(int id)
+        {
+            var student = repository.Get(id);
+            if (student == null)
+            {
+                throw new ArgumentException("Студент с указанным ID не найден");
+            }
+            return new List<string>
+            {
+                student.Id.ToString(),
+                student.Name,
+                student.Speciality,
+                student.Group
+            };
+        }
+        /// <summary>
+        /// Добавление студента
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="speciality">Специальность</param>
+        /// <param name="group">Группа</param>
+        public void AddStudent(int id,string name, string speciality, string group)
+        {
+            if (name == string.Empty|| speciality == string.Empty || group == string.Empty) { throw new ArgumentException("Одно из полей студента пустое!"); }
+            else
+            {
+                Student student = new Student()
+                {
+                    Name = name,
+                    Speciality = speciality,
+                    Group = group
+                };
+                repository.Create(student);
+            }
+        }
+        /// <summary>
+        /// Удаление студента
+        /// </summary>
+        /// <param name="name">Имя</param>
+        public void RemoveStudent(int id)
+        {
+            var student = repository.Get(id);
+            if (student == null)
+            {
+                throw new ArgumentException("Студент с указанным ID не найден");
+            }
+            repository.Delete(id);
+            if (repository.GetAll().Count() == 0)
+            {
+                ResetStudentIdSequence();
+            }
+        }
+        /// <summary>
+        /// Обновление параметров студента.
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="newSpeciality">Новая Специальность</param>
+        /// <param name="newGroup">Новая Группа</param>
+        public void UpdateStudent(int id, string newname, string newSpeciality, string newGroup)
+        {
+            var student = repository.Get(id);
+            if (student == null)
+            {
+                throw new ArgumentException("Студент с указанным ID не найден");
+            }
+
+            if (!string.IsNullOrEmpty(newname))
+            {
+                student.Name = newname;
+            }
+            if (!string.IsNullOrEmpty(newSpeciality))
+            {
+                student.Speciality = newSpeciality;
+            }
+            if (!string.IsNullOrEmpty(newGroup))
+            {
+                student.Group = newGroup;
+            }
+
+            repository.Update(student);
+        }
+        /// <summary>
+        /// Метод возвращающий список всех студентов
+        /// </summary>
+        /// <returns>Список студентов</returns>
+        public List<List<string>> GetAllStudents()
+        {
+            var students = repository.GetAll();
+            return students.Select(student => new List<string>
+            {
+                student.Id.ToString(),
+                student.Name,
+                student.Speciality,
+                student.Group
+            }).ToList();
+        }
+        /// <summary>
+        /// Метод возвращающий словарь на основе специальностей
+        /// </summary>
+        /// <returns>Словарь string,int</returns>
+        public Dictionary<string, int> GetSpecialityDistribution()
+        {
+            var students = repository.GetAll();
+            if (!students.Any())
+            {
+                throw new InvalidOperationException("Нет данных о студентах");
+            }
+
+            return students.GroupBy(s => s.Speciality)
+                           .ToDictionary(g => g.Key, g => g.Count());
+        }
+        /// <summary>
+        /// Метод сброса порядкового номера ID
+        /// </summary>
+        public void ResetStudentIdSequence()
+        {
+            if (repository is DapperRepository<Student> dapperRepository)
+            {
+                dapperRepository.ExecuteSQL(@"
+            SELECT setval(pg_get_serial_sequence('students', 'id'), 
+            (SELECT COALESCE(MAX(id),0) FROM students), false);");
+            }
+            else
+            {
+                throw new NotSupportedException("Текущий репозиторий не поддерживает прямое выполнение SQL-запросов.");
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/DapperRepository.cs
@@ -0,0 +1,112 @@
+﻿using Dapper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DataAccessLayer
+{
+    public class DapperRepository<T> : IRepository<T> where T : Student, IDomainObject, new()
+    {
+        private static string connectionString = "Host=localhost;Port=5432;Database=students;Username=postgres;Password=David800;";
+        private NpgsqlConnection db = new NpgsqlConnection(connectionString);
+
+        /// <summary>
+        /// Создает новую запись в базе данных на основе переданного объекта.
+        /// </summary>
+        /// <param name="t">Объект для создания в базе данных.</param>
+        public void Create(T t)
+        {
+            string sqlQuery = "INSERT INTO Students (Name, \"Group\", Speciality) " +
+                      "VALUES (@Name, @Group, @Speciality) RETURNING Id";
+
+            int id = db.ExecuteScalar<int>(sqlQuery, new
+            {
+                Name = t.Name,
+                Group = t.Group,
+                Speciality = t.Speciality
+            });
+
+            t.Id = id;
+        }
+
+        /// <summary>
+        /// Возвращает все записи из таблицы Students.
+        /// </summary>
+        /// <returns>Коллекция всех объектов типа T из базы данных.</returns>
+        public IEnumerable<T> GetAll()
+        {
+            return db.Query<T>("SELECT * FROM Students");
+        }
+
+        /// <summary>
+        /// Возвращает запись из базы данных по указанному идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор искомой записи.</param>
+        /// <returns>Объект типа T, соответствующий указанному идентификатору, или null, если запись не найдена.</returns>
+        public T Get(int id)
+        {
+            return db.QueryFirstOrDefault<T>("SELECT * FROM Students WHERE Id = @Id", new { Id = id });
+        }
+
+        /// <summary>
+        /// Обновляет существующую запись в базе данных.
+        /// </summary>
+        /// <param name="entity">Объект с обновленными данными.</param>
+        public void Update(T entity)
+        {
+            string sqlQuery = "UPDATE Students SET Name = @Name, \"Group\" = @Group, Speciality = @Speciality WHERE Id = @Id";
+            db.Execute(sqlQuery, entity);
+        }
+
+        /// <summary>
+        /// Удаляет запись из базы данных по указанному идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор записи для удаления.</param>
+        public void Delete(int id)
+        {
+            db.Execute("DELETE FROM Students WHERE Id = @Id", new { Id = id });
+        }
+
+        /// <summary>
+        /// Сохраняет изменения в базе данных. В текущей реализации с Dapper этот метод не выполняет никаких действий.
+        /// </summary>
+        public void Save()
+        {
+            // В случае с Dapper, Save() метод не требуется, так как изменения сохраняются сразу
+            // Этот метод оставлен пустым для совместимости с интерфейсом
+        }
+
+        /// <summary>
+        /// Освобождает ресурсы, используемые объектом DapperRepository.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Освобождает неуправляемые (а при необходимости и управляемые) ресурсы.
+        /// </summary>
+        /// <param name="disposing">Значение true для освобождения управляемых и неуправляемых ресурсов; значение false для освобождения только неуправляемых ресурсов.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db?.Dispose();
+            }
+        }
+        public void ExecuteSQL(string sql)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute(sql);
+            }
+        }
+    }
+}
