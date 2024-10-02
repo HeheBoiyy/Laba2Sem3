afe43a01614d069d3a889fe268422514e37681df
HeheBoiyy <school152qq@mail.ru>
2024-10-03 01:17:15 +0700

    Добавлено summary для context.cs

--- DataAccessLayer/Context.cs
+++ DataAccessLayer/Context.cs
@@ -8,15 +8,31 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
+    /// <summary>
+    /// Представляет контекст базы данных для приложения, обеспечивающий доступ к данным студентов.
+    /// Этот класс настраивает подключение к базе данных и определяет структуру таблицы Students.
+    /// </summary>
     public class AppDbContext : DbContext
     {
+        /// <summary>
+        /// Получает или устанавливает DbSet студентов. Это представляет таблицу Students в базе данных.
+        /// </summary>
         public DbSet<Student> Students { get; set; }
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класса AppDbContext.
+        /// </summary>
+        /// <param name="options">Параметры, используемые DbContext.</param>
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             ChangeTracker.AutoDetectChangesEnabled = true;
         }
 
+        /// <summary>
+        /// Настраивает модель, которая была обнаружена по соглашению из типов сущностей,
+        /// представленных в свойствах DbSet в производном контексте.
+        /// </summary>
+        /// <param name="modelBuilder">Построитель, используемый для конструирования модели для этого контекста.</param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -29,7 +45,6 @@ namespace DataAccessLayer
                 entity.Property(e => e.Speciality).HasColumnName("speciality");
                 entity.Property(e => e.Group).HasColumnName("Group");
             });
-
         }
     }
 }
